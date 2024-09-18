474605f4f57c09be4c3334154503e05b2f6ef3e0
谷口虎太郎 <taniguchi.kotachan@gmail.com>
2024-09-18 15:42:42 +0900

    敵のランダム生成

--- Assets/kotaro/enemysponescript.cs
+++ Assets/kotaro/enemysponescript.cs
@@ -6,6 +6,9 @@ using UnityEngine.UIElements;
 public class enemysponescript : MonoBehaviour
 {
     [SerializeField] GameObject enemycar;
+    [SerializeField] float spown = 1;
+    [SerializeField] private Vector2 shotPoint;
+    [SerializeField] float adjustment;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +18,31 @@ public class enemysponescript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       if (Input.GetMouseButtonDown(0))
+        {
+            spawnenemy();
+        }
     }
 
-    void spownenemy()
+    void spawnenemy()
     {
-
+        
+        Vector2 spawnpos;
+        int random = UnityEngine.Random.Range(0, 3);
+        if(random==0)
+        {
+            spawnpos = shotPoint;
+            spawnpos.x = -adjustment;
+        }
+        else if(random==1)
+        {
+            spawnpos = shotPoint;
+        }
+        else
+        {
+            spawnpos = shotPoint;
+            spawnpos.x = adjustment;
+        }
+        Instantiate(enemycar, spawnpos, Quaternion.Euler(0, 0, 0));
     }
 }
