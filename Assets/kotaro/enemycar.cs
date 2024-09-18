02b98164fd9ef5af8fda0dd0965b131228f1f88c
谷口虎太郎 <taniguchi.kotachan@gmail.com>
2024-09-18 14:20:58 +0900

    エネミーの動きを追加

--- Assets/kotaro/enemycar.cs
+++ Assets/kotaro/enemycar.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class enemycar : MonoBehaviour
 {
+    [SerializeField] float movespeed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 pos = transform.position;
+        pos.y += movespeed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hit");
     }
 }
---
+++ Assets/kotaro/enemysponescript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class enemysponescript : MonoBehaviour
+{
+    [SerializeField] GameObject enemycar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void spownenemy()
+    {
+
+    }
+}
