a6d542f4ca8649a5fc78f67768b0aa4772e4c683
2240046S\2240046 <2240046@ecc.ac.jp>
2024-09-18 18:35:39 +0900

    自機操作完成です。

--- Assets/Matsumura/Player.cs
+++ Assets/Matsumura/Player.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
 
-    public float speed = 10.0f;
-    public float rotation = 30.0f;
-    public float rotateSpeed=1.0f;   //変化する角度
-    public float rotationTwo = 0;    //二つの角度
-    public float time = 0.0f;
-    [SerializeField] float timespeed = 3.0f;
+    public float speed = 10.0f;                         //x軸の変化量
+    public float rotation = 30.0f;                      //角度
+    public float startRotation = 0;                     //角度の初期値
+    private float oldRotation = 0.0f;                   //前回の回転値
+    private float moveRateVolume = 0.0f;                //ボタンが押されている際の回転値
+    private float stopRateVolume = 0.0f;　              //ボタンが押されていない時の回転値
+    [SerializeField] float moveRotationSpeed = 3.0f;　  //ボタンが押された際の回転スピード
+    [SerializeField] float ReturnRotationSpeed = 5.0f;  //ボタンが押されていない時の回転スピード
+    [SerializeField] float rate = 0.0f;               　//回転補完計算結果
 
     // Start is called before the first frame update
     void Start()
     {
+        startRotation = 0.0f;
     }
 
     // Update is called once per frame
@@ -23,28 +28,67 @@ public class Player : MonoBehaviour
         Vector2 pos = transform.position;
         Vector3 angle = transform.eulerAngles;
 
+        //ボタンが押された場合
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            //lerpの割合を更新
+            rate = Mathf.Clamp01(moveRateVolume += moveRotationSpeed * Time.deltaTime);
+        }
+
         //動く処理
+        //同時に押された場合
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            angle.z = PlayerReturnRotation();
+        }
         //左に動く
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             pos.x -= speed * Time.deltaTime;
-            time += timespeed * Time.deltaTime;
-            angle.z = Mathf.Lerp(rotationTwo, rotation, time);
-            //angle.z = rotation;
+            angle.z = PlayerLeftRotation();
         }
         //右に動く
         else if (Input.GetKey(KeyCode.D))
         {
             pos.x += speed * Time.deltaTime;
-            time += timespeed * Time.deltaTime;
-            angle.z = Mathf.Lerp(rotationTwo, -rotation, time);
-            //angle.z = -rotation;
+            angle.z = PlayerRightRotation();
         }
         //何もキーが押されていない時
-        else { angle.z = 0;time = 0; }
+        else
+        {
+            angle.z = PlayerReturnRotation();
+        }
 
         transform.position = pos;
         transform.eulerAngles = angle;
+    }
+
+    //右の回転処理
+    float PlayerRightRotation()
+    {
+        oldRotation = -rotation;
+        stopRateVolume = 0.0f;
+        return Lerp(startRotation, -rotation);
+    }
+    //左の回転処理
+    float PlayerLeftRotation()
+    {
+        oldRotation = rotation;
+        stopRateVolume = 0.0f;
+        return Lerp(startRotation, rotation);
+    }
+    //正面に戻る回転処理
+    float PlayerReturnRotation()
+    {
+        moveRateVolume = 0;
+        rate = Mathf.Clamp01(stopRateVolume += ReturnRotationSpeed * Time.deltaTime);
+        return Lerp(oldRotation, 0.0f);
+    }
+
+    //補完計算関数
+    float Lerp(float pos, float inverse)
+    {
+        return Mathf.Lerp(pos, 1.0f * inverse, rate);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
