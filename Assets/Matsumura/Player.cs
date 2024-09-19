bef77c217deae76b5d23e280db0efe879e7c52c6
2240046S\2240046 <2240046@ecc.ac.jp>
2024-09-19 16:52:26 +0900

    カメラシェイク、自機が壁に当たると速度減、速度０以下にならないようにしました

--- Assets/Matsumura/Player.cs
+++ Assets/Matsumura/Player.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking.Types;
 using UnityEngine.UIElements;
+using Cinemachine;
 
 public class Player : MonoBehaviour
 {
-    public float brake = 7;                             //ブレーキ用
-    public float acceleration = 7;                   //加速度      
+    public float acceleration = 10;                   //加速度      
     public float initialVelocity = 60;                  //初速
     public float speed = 10.0f;                         //x軸の変化量
     public float speedY = 0;                            //道路の速度変化量
     public float deceleration = 3.0f;                  //減速
     public float rotation = 30.0f;                      //角度
     public float startRotation = 0;                     //角度の初期値
+    private float brake = 18;                             //ブレーキ用
     private float oldRotation = 0.0f;                   //前回の回転値
     private float moveRateVolume = 0.0f;                //ボタンが押されている際の回転値
     private float stopRateVolume = 0.0f;　              //ボタンが押されていない時の回転値
     [SerializeField] float moveRotationSpeed = 3.0f;　  //ボタンが押された際の回転スピード
     [SerializeField] float ReturnRotationSpeed = 5.0f;  //ボタンが押されていない時の回転スピード
-    [SerializeField] float rate = 0.0f;               　//回転補完計算結果
+    [SerializeField] float rate = 0.0f;                 //回転補完計算結果
+    [SerializeField] GameObject came;
+    bool cameraChine = false;
 
     public float test = 0;
 
@@ -30,6 +33,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         startRotation = 0.0f;
+        speedY = 60;
     }
 
     private void FixedUpdate()
@@ -38,16 +42,25 @@ public class Player : MonoBehaviour
         if (leftClick)
         {
             Debug.Log("加速");
+            //float speed = 100.0f - speedY;
+            //speed /= 100.0f;
+            //speed *= speed;
+            if (speedY <= 50)
+            {
+                acceleration = 20;
+            }
+            else
+                acceleration = 10;
             speedY += acceleration * Time.deltaTime;
         }
         //右マウス減速
         else if(rightClick)
         {
             speedY -= (deceleration+brake) * Time.deltaTime;
+            speedY = Mathf.Max(speedY, 0);
         }
         else
         {
-            Debug.Log("減速");
             speedY -= deceleration * Time.deltaTime;
             speedY = Mathf.Max(speedY, 0);
         }
@@ -100,7 +113,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        //カメラシェイク
+       if(cameraChine)
+        {
+            
+         
+        }
+        //cameraChine = false;
     }
 
     //右の回転処理
@@ -134,5 +153,16 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Hit");
+    }
+    //壁にあたる判定
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("当たり！");
+        //カメラシェイク
+        var inpulseSource = came.GetComponent<CinemachineImpulseSource>();
+        inpulseSource.GenerateImpulse();
+        //減速
+        speedY -= (acceleration+30 )* Time.deltaTime;
+        speedY = Mathf.Max(speedY, 0);
     }
 }
