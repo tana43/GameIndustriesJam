7c92a145357ece638799a6b3aa5ed1c4d0766ca0
谷口虎太郎 <taniguchi.kotachan@gmail.com>
2024-09-19 11:16:35 +0900

    敵が近づいたときに減速させるようにしました

--- Assets/kotaro/collisionbox.cs
+++ Assets/kotaro/collisionbox.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class collisionbox : MonoBehaviour
 {
+    [SerializeField] private float time = 0; 
+    private bool collisionflag = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(collisionflag)
+        {
+            transform.parent.GetComponent<enemycar>().movespeed = Mathf.Lerp( transform.parent.GetComponent<enemycar>().movespeed, 1, time);
+
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("hit");
-        transform.parent.GetComponent<enemycar>().movespeed = 1;
+        collisionflag = true;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        time = time + Time.deltaTime/20;
     }
 }
